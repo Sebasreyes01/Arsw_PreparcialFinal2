5f1f6800ecb1b41adc73e4d3cc2faee3f42f68e6
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-11-29 16:29:14 -0500

    First commit

---
+++ src/main/java/com/edu/eci/arsw/preparcial2/Preparcial2Application.java
@@ -0,0 +1,12 @@
+package com.edu.eci.arsw.preparcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Preparcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Preparcial2Application.class, args);
+	}
+}
---
+++ src/test/java/com/edu/eci/arsw/preparcial2/Preparcial2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.eci.arsw.preparcial2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Preparcial2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
